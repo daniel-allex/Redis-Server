8e7f477c371d333406a2845d770bc8f42fb11bb5
root <root@LAPTOP-G4RJJCES>
2024-05-28 18:53:36 -0400

    added handling multiple pings

--- app/TCP_connection.go
+++ app/TCP_connection.go
@@ -1,18 +1,63 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"net"
+)
 
-func Write(conn net.Conn, message string) error {
-	_, err := conn.Write([]byte(message))
+type TCPConnection struct {
+	conn *net.Conn
+	io   *bufio.ReadWriter
+}
+
+func NewTCPConnection(conn *net.Conn) *TCPConnection {
+	reader := bufio.NewReader(*conn)
+	writer := bufio.NewWriter(*conn)
+
+	readWriter := bufio.NewReadWriter(reader, writer)
+	return &TCPConnection{conn: conn, io: readWriter}
+}
+
+func AcceptTCPConnection(listener net.Listener) (*TCPConnection, error) {
+	conn, err := listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	return NewTCPConnection(&conn), nil
+}
+
+func (conn *TCPConnection) ReadLine() (string, error) {
+	return conn.io.ReadString('\n')
+}
+
+func (conn *TCPConnection) Read() (string, error) {
+	buf := make([]byte, 128)
+	n, err := conn.io.Read(buf)
+
+	return string(buf[:n]), err
+}
+
+func (conn *TCPConnection) Write(message string) error {
+	_, err := conn.io.WriteString(message)
+	if err != nil {
+		return err
+	}
+
+	err = conn.io.Flush()
 	return err
 }
 
-func WriteLine(conn net.Conn, message string) error {
-	err := Write(conn, message+"\n")
+func (conn *TCPConnection) WriteLine(message string) error {
+	err := conn.Write(message + "\n")
 	return err
 }
 
-func Respond(conn net.Conn, message string) error {
-	err := WriteLine(conn, "+"+message+"\r")
+func (conn *TCPConnection) Respond(message string) error {
+	err := conn.WriteLine("+" + message + "\r")
 	return err
+}
+
+func (conn *TCPConnection) Close() error {
+	return (*conn.conn).Close()
 }
--- app/server.go
+++ app/server.go
@@ -2,31 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
 
+func handleClient(conn *TCPConnection) {
+	defer conn.Close()
+
+	for {
+		input, err := conn.Read()
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading input: ", err.Error())
+			os.Exit(1)
+		}
+
+		if len(input) == 0 {
+			return
+		}
+
+		if strings.Contains(input, "PING") {
+			err = conn.Respond("PONG")
+
+			if err != nil {
+				fmt.Println("Error responding to connection: ", err.Error())
+				os.Exit(1)
+			}
+		}
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+
+	conn, err := AcceptTCPConnection(listener)
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	err = Respond(conn, "PONG")
-	if err != nil {
-		fmt.Println("Error responding to connection: ", err.Error())
-		os.Exit(1)
-	}
+	handleClient(conn)
+
 }
