923c7519701b68ad04b9a2c1057020a3413cee90
root <root@LAPTOP-G4RJJCES>
2024-07-11 01:24:17 -0400

    codecrafters submit [skip ci]

--- app/TCP_connection.go
+++ app/TCP_connection.go
@@ -32,7 +32,7 @@ func (conn *TCPConnection) ReadLine() (string, error) {
 }
 
 func (conn *TCPConnection) Read() (string, error) {
-	buf := make([]byte, 2045)
+	buf := make([]byte, 8000)
 	n, err := conn.io.Read(buf)
 
 	return string(buf[:n]), err
